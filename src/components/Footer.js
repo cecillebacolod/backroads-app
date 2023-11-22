c9af89a0f8771cfffd31c183d94b2c1e310755dd
Cecille Bacolod <cecille.bacolod@racnet.com.ph>
2023-11-22 20:03:31 +0800

    first commit

---
+++ src/components/Footer.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { socialLinks } from '../data.js'
+import PageLinks from './PageLinks.js'
+import SocialLink from './SocialLink.js'
+
+const Footer = () => {
+  return (
+    <footer className="section footer">
+      <PageLinks parentClass="footer-links" itemClass="footer-link" />
+      <ul className="footer-icons">
+        {socialLinks.map((link) => {
+          return <SocialLink key={link.id} {...link} itemClass="footer-icon" />
+        })}
+      </ul>
+      <p className="copyright">
+        copyright &copy; Backroads travel tours company
+        <span id="date">{new Date().getFullYear()}</span> all rights reserved
+      </p>
+    </footer>
+  )
+}
+
+export default Footer
